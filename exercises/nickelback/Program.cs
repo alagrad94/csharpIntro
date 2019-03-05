2b943f50209009782499920c7b901471bf34756a
Russell Reiter <russell.reiter@gmail.com>
2019-03-05 12:47:49 -0600

    Exercises Complete through Chapter 13

---
+++ exercises/nickelback/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickelback {
+
+  class Program {
+
+    static void Main() {
+
+      List<Dictionary<string, string>> goodSongs = new List<Dictionary<string, string>>();
+
+      HashSet<Dictionary<string, string>> allSongs = new HashSet<Dictionary<string, string>>();
+      allSongs.Add(new Dictionary<string, string>(){{"The Eagles", "Seven Bridges Road"}});
+      allSongs.Add(new Dictionary<string, string>(){{"Jimmy Buffet", "Margaritaville"}});
+      allSongs.Add(new Dictionary<string, string>(){{"Jimmy Buffet", "Come Monday"}});
+      allSongs.Add(new Dictionary<string, string>(){{"The Eagles", "Hotel California"}});
+      allSongs.Add(new Dictionary<string, string>(){{"Nickelback", "Lullaby"}});
+      allSongs.Add(new Dictionary<string, string>(){{"Nickelback", "Rockstar"}});
+      allSongs.Add(new Dictionary<string, string>(){{"Nickelback", "Photograph"}});
+
+      foreach (Dictionary<string, string> song in allSongs) {
+        foreach (KeyValuePair<string, string> item in song) {
+          if(item.Key != "Nickelback") {
+            goodSongs.Add(song);
+          }
+        }
+      }
+
+      foreach (Dictionary<string, string> song in goodSongs) {
+        foreach (KeyValuePair<string, string> item in song) {
+          Console.WriteLine($"{item.Value} is an awesome song by {item.Key}");
+        }
+      }
+    }
+  }
+}
